ef2a55f7b46c06a39f7f96f118ca3cc82340cc33
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-03-15 10:54:31 -0500

    updating readme

---
+++ examples/Pca9685-Light-Example/Pca9685-Light-Example.ino
@@ -0,0 +1,26 @@
+#include "Pca9685.h"
+#include "Pca9685Board.h"
+
+#define NO_OF_BOARDS 3
+
+int MAX_PINS = 0;
+
+Pca9685Board pca9685Board;
+void setup() {
+  Serial.begin(9600);
+  pca9685Board.initPca9685Boards(NO_OF_BOARDS);
+  MAX_PINS = NO_OF_BOARDS * 16;
+}
+
+void loop() {
+  for (int i = 0; i <= MAX_PINS; i++) {
+    pca9685Board.switchOn(i);
+  }
+  pca9685Board.displayPinState();
+  delay(1000);
+  for (int i = 0; i <= MAX_PINS; i++) {
+    pca9685Board.switchOff(i);
+  }
+  pca9685Board.displayPinState();
+  delay(1000);
+}
